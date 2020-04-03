28524284de5134b9e7e41827e556d902ef4de8fe
Abhishek <61415942+abhi9960@users.noreply.github.com>
2020-04-03 16:16:28 +0530

    Update compiler.sh

--- triton-script/compiler.sh
+++ triton-script/compiler.sh
@@ -2,7 +2,7 @@
 export DEBIAN_FRONTEND=noninteractive
 export TZ=Asia/Kolkata
 export TIME=$(date +"%S-%F")
-export ZIPNAME=Triton-Ebella-${TIME}
+export ZIPNAME=MIONEER-${TIME}
 ln -fs /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
@@ -67,7 +67,7 @@ apt-get update -qq && \
 	zlib1g-dev \
 	zstd
 
-git clone --depth=1 -b aosp/EAS-3.18 https://github.com/fadlyas07/android-kernel-xiaomi-msm8917-1 --single-branch triton && cd triton
+git clone -b aosp/EAS-3.18 https://github.com/fadlyas07/android-kernel-xiaomi-msm8917-1 --single-branch triton && cd triton
 git clone https://github.com/Thagoo/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 -b lineage-17.0 tc
 git clone https://github.com/Thagoo/AnyKernel3
 echo cloning done
